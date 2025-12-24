32dba5e8f49dc9cc7f776b856676fc25d56ccc20
Sachin <1eyedipatch@gmail.com>
2025-12-24 17:20:53 +0530

    day_2

--- day1.py
+++ day1.py
@@ -1,2 +1,10 @@
-name=input("Enter your name:")
-print("name")
+name = input("Enter your name:")
+age = input("Enter your age:")
+print("Hello " + name + " age " + age)
+if int(age) > 18:
+    print("Adult")
+else:
+    print("MUEHEHEHEHE")
+
+brty = 2025 - int(age)
+print("Birthyear ", brty)
